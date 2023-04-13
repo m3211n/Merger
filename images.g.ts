229fca14292d7fd990356864f01d7d61b068fee4
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-13 16:49:06 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,190 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tile_10_0":return img`
+. . . . . . . . . . . . 
+. . 1 1 1 1 1 1 1 1 . . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 . 
+. . 1 1 1 1 1 1 1 1 . . 
+. . . . . . . . . . . . 
+`;
+            case "image2":
+            case "tile_10_1":return img`
+. . . . . . . . . . . . 
+. . d d d d d d d d . . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. d d d d d d d d d d . 
+. . d d d d d d d d . . 
+. . . . . . . . . . . . 
+`;
+            case "image3":
+            case "tile_10_2":return img`
+. . . . . . . . . . . . 
+. . b b b b b b b b . . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. b b b b b b b b b b . 
+. . b b b b b b b b . . 
+. . . . . . . . . . . . 
+`;
+            case "image4":
+            case "tile_10_3":return img`
+. . . . . . . . . . . . 
+. . c c c c c c c c . . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. c c c c c c c c c c . 
+. . c c c c c c c c . . 
+. . . . . . . . . . . . 
+`;
+            case "image5":
+            case "tile_10_4":return img`
+. . . . . . . . . . . . 
+. . a a a a a a a a . . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. a a a a a a a a a a . 
+. . a a a a a a a a . . 
+. . . . . . . . . . . . 
+`;
+            case "image6":
+            case "tile_10_5":return img`
+. . . . . . . . . . . . 
+. . 8 8 8 8 8 8 8 8 . . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 . 
+. . 8 8 8 8 8 8 8 8 . . 
+. . . . . . . . . . . . 
+`;
+            case "image7":
+            case "tile_10_6":return img`
+. . . . . . . . . . . . 
+. . 6 6 6 6 6 6 6 6 . . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. 6 6 6 6 6 6 6 6 6 6 . 
+. . 6 6 6 6 6 6 6 6 . . 
+. . . . . . . . . . . . 
+`;
+            case "image8":
+            case "tile_10_7":return img`
+. . . . . . . . . . . . 
+. . 7 7 7 7 7 7 7 7 . . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 7 7 7 7 7 . 
+. . 7 7 7 7 7 7 7 7 . . 
+. . . . . . . . . . . . 
+`;
+            case "image9":
+            case "tile_10_8":return img`
+. . . . . . . . . . . . 
+. . 5 5 5 5 5 5 5 5 . . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 . 
+. . 5 5 5 5 5 5 5 5 . . 
+. . . . . . . . . . . . 
+`;
+            case "image10":
+            case "tile_10_9":return img`
+. . . . . . . . . . . . 
+. . 4 4 4 4 4 4 4 4 . . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. 4 4 4 4 4 4 4 4 4 4 . 
+. . 4 4 4 4 4 4 4 4 . . 
+. . . . . . . . . . . . 
+`;
+            case "image11":
+            case "tile_10_10":return img`
+. . . . . . . . . . . . 
+. . . . . . . . . . . . 
+. . . . . . e e . . . . 
+. . . . . e 5 e . . . . 
+. . e e e 4 5 e e e . . 
+. . e 5 5 5 5 5 5 e . . 
+. . . e 4 5 5 4 e . . . 
+. . . e 5 4 4 5 e . . . 
+. . e 5 4 e e 4 5 e . . 
+. . e e e . . e e e . . 
+. . . . . . . . . . . . 
+. . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
